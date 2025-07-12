45b0d7ed19c0785437864b01a92a665b72b3f8fb
Italo Nascimento <58990025+italojj@users.noreply.github.com>
2025-07-11 21:14:40 -0300

    alteration

---
+++ Cliente.cpp
@@ -0,0 +1,23 @@
+#include "Cliente.h"
+#include <iostream>
+
+using namespace std;
+
+Cliente::Cliente(int id, string nome, string cpf, string dataCadastro) : Pessoa(nome,cpf), idCliente(id) dataCadastro(dataCadastro){
+
+}
+
+int Cliente::getIdCliente() const{
+    return this->idCliente
+}
+string Cliente::getDatacadastro() const{
+    return this->dataCadastro
+}
+
+void Cliente::exibirDetalhes() const{
+    cout << "Detalhes do cliente: " << endl;
+    cout << "ID do cliente: " << this->idCliente << endl;
+    cout << "Nome: " << getNome() << endl;
+    cout << "CPF: " << getCpf() <<endl;
+    cout << "Data de cadastro" << this-> dataCadastro << endl;
+}
