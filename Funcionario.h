7e7f39dee1f548f3dde40be51a5197e4590f605d
Italo Nascimento <italo2004v@gmail.com>
2025-07-15 21:02:46 -0300

    modify

--- Funcionario.h
+++ Funcionario.h
@@ -10,7 +10,7 @@ private:
     double salario;
 
 public:
-    Funcionario(std::string nome, std::string cpf, std::string cargo, double salario); // construtor
+    Funcionario(std::string nome, std::string cpf, std::string cargo, double salario); // construtor para funcionarios
 
     //Getters da classe funcionario, atributos específicos
     std::string getCargo() const;
