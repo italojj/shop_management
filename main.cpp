348f66c9dbe128aabe4fbb9e8a7873ac59840fd6
Italo Nascimento <italo2004v@gmail.com>
2025-07-16 12:46:17 -0300

    tests update

--- main.cpp
+++ main.cpp
@@ -11,14 +11,14 @@ using namespace std;
 int main() {
     cout << "Testes: " << endl;
 
-    // --- Testando com Clientes ---
+    //Testando com Clientes
     cout << "\n Gerenciando Clientes" << endl;
     GerenciadorDados<Cliente> gerenciador_clientes;
 
-    Cliente c1(1, "Joao da Silva", "111.222.333-44", "15/07/2025");
-    Cliente c2(2, "Ana Costa", "555.666.777-88", "15/07/2025");
+    Cliente c1(1, "Joao da Silva", "111.222.333-44", "11/07/2025");
+    Cliente c2(2, "Italo Nascimento", "555.666.777-88", "11/07/2025");
     
-    cout << "Adicionando clientes..." << endl;
+    cout << "Adicionando clientes." << endl;
     gerenciador_clientes.adicionar(c1);
     gerenciador_clientes.adicionar(c2);
 
